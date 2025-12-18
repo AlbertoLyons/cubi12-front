6ca8bf13fc59700962bb91435ef5069bd19eb814
bemoremore <113298256+bemoremore@users.noreply.github.com>
2025-12-17 21:08:07 -0300

    fix: update API URL and button label on HomePage

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -43,7 +43,7 @@ const HomePage = () => {
       />
       <Link to={"/interactive-mesh"}>
         <BlueButton variant="contained" size="large">
-          MALLA GENiAL
+          MALLA EXCLESe
         </BlueButton>
       </Link>
 
